cf09ee82b127ed347fe5e73c02054bbe7be20728
Scott G. Daniel <scottdaniel25@gmail.com>
2018-06-05 23:21:51 +0000

    complete restructafoldering

--- 000-START-HERE/stampede/test.sh
+++ 000-START-HERE/stampede/test.sh
@@ -2,21 +2,22 @@
 
 #SBATCH -A iPlant-Collabs
 #SBATCH -N 1
-#SBATCH -n 1
+#SBATCH -n 12
 #SBATCH -t 02:00:00
 #SBATCH -p development
-#SBATCH -J cntrfuge
+#SBATCH -J deseq2
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user scottdaniel@email.arizona.edu
 
 #for local testing#####
 #if the singularity.conf is right, then /vagrant should be auto-shared
-#export WORK="/vagrant"
-export WORK="$HOME/singularity-vm"
+export WORK="/vagrant"
+export BAMS_DIR="$WORK/bams"
+export COUNT_DIR="$WORK/counts"
 export GFF_DIR="$wORK/gffs" #also has genome.fa
 ########################
 
-export OUT_DIR="$WORK/cuffdiff_test"
+export OUT_DIR="$WORK/deseq_test"
 
 #export MY_PARAMRUN="$HOME/launcher/paramrun"
 
@@ -24,7 +25,8 @@ export OUT_DIR="$WORK/cuffdiff_test"
 
 #-i "$WORK/genomes"
 
-cuffdiff $GFF_DIR/transcripts.gtf \
-    -C sample-sheet.txt \
+bash run.sh -g $GFF_DIR/all.RefSeq.gff \
+    -b $BAMS_DIR
+    -t $COUNT_DIR/target.txt \
     -o $OUT_DIR \
     -p 4
