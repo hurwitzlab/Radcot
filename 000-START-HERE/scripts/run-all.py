8f7c096bd33ed44a0578fda20d1e468fba6bf0b4
Scott Daniel <scottdaniel25@gmail.com>
2018-06-06 16:51:24 -0700

    sticking with the format

---
+++ 000-START-HERE/scripts/run-all.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""Boilerplate stuff
+This script essentially just parses out the metadata file
+and the options files
+and feeds them to each of the steps
+"""
+
+import os, sys, argparse
+import plumbum
+import pandas as pd
+
+#setup arguments
+
+#functions
+
+def parse_metadata(metadata_file):
+    return None
+
+def run_centrifuge(reads, options):
+    Status = ''
+
+    #do stuff
+
+    return Status
+
+def run_rna_align(reads, options):
+    Status = ''
+
+    # do stuff
+
+    return Status
+
+def run_htseq(alignments, options):
+    Status = ''
+
+    # do stuff
+
+    return Status
+
+#main
+
