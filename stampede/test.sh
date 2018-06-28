132c7e226a8c4d0b1511752356a7da63d3696187
Scott G. Daniel <scottdaniel25@gmail.com>
2018-06-28 17:28:27 -0500

    default deseq2 to output to OUTPUT_DIR

--- stampede/test.sh
+++ stampede/test.sh
@@ -3,13 +3,15 @@
 #SBATCH -A iPlant-Collabs
 #SBATCH -N 1
 #SBATCH -n 68
-#SBATCH -t 12:00:00
-#SBATCH -p normal
+#SBATCH -t 2:00:00
+#SBATCH -p development
 #SBATCH -J radcot
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user scottdaniel@email.arizona.edu
 
 export INPUT_DIR="$WORK/radcot-data"
+#export OUT_DIR="$WORK/quick-test"
+#export GENOME_DIR="$WORK/gtemp"
 export OUT_DIR="$WORK/test-of-radcot"
 export GENOME_DIR="$WORK/genomes"
 
@@ -18,5 +20,7 @@ bash run.sh -o $OUT_DIR \
     -g $GENOME_DIR \
     -m $WORK/subset_crc_mouse_metadata_simreps.txt \
     --debug --threads 12 \
-    --procs 4
+    --procs 4 \
+    --skip-centrifuge \
+    --skip-rna-align
 
