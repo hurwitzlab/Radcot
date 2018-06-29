ce8d248f2d49118bf5e9035ab5670784eb49ccda
Scott G. Daniel <scottdaniel25@gmail.com>
2018-06-29 11:35:10 -0500

    last test

--- stampede/test.sh
+++ stampede/test.sh
@@ -3,16 +3,14 @@
 #SBATCH -A iPlant-Collabs
 #SBATCH -N 1
 #SBATCH -n 68
-#SBATCH -t 2:00:00
-#SBATCH -p development
+#SBATCH -t 12:00:00
+#SBATCH -p normal
 #SBATCH -J radcot
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user scottdaniel@email.arizona.edu
 
 export INPUT_DIR="$WORK/radcot-data"
-#export OUT_DIR="$WORK/quick-test"
-#export GENOME_DIR="$WORK/gtemp"
-export OUT_DIR="$WORK/test-of-radcot"
+export OUT_DIR="$WORK/LAST_TEST"
 export GENOME_DIR="$WORK/genomes"
 
 bash run.sh -o $OUT_DIR \
@@ -20,7 +18,4 @@ bash run.sh -o $OUT_DIR \
     -g $GENOME_DIR \
     -m $WORK/subset_crc_mouse_metadata_simreps.txt \
     --debug --threads 12 \
-    --procs 4 \
-    --skip-centrifuge \
-    --skip-rna-align
-
+    --procs 4
